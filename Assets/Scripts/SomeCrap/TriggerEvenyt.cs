66c83cbd38158bbfceeae4dc53fd685318191c95
BlankRip <aniketh_g5@outlook.com>
2020-05-03 22:28:30 +0400

    Teaching Itaza some stuff

---
+++ Assets/Scripts/SomeCrap/TriggerEvenyt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvenyt : MonoBehaviour
+{
+    public Material changMatTo;
+    public float forceToAdd;
+    Rigidbody currentRb;
+    Renderer currentRenderer;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        currentRenderer = other.gameObject.GetComponent<Renderer>();
+        currentRb = other.gameObject.GetComponent<Rigidbody>();
+        currentRenderer.material = changMatTo;
+        currentRb.AddForce(Vector3.up * forceToAdd, ForceMode.Impulse);
+
+    }
+}
