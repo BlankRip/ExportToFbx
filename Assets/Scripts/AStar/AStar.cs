ff3d69723d253386dfdef3532dbf71b4ca443f41
BlankRip <aniketh_g5@outlook.com>
2020-07-23 17:16:50 +0400

    Moved cost and vised and from to node class and removed form Astar code

--- Assets/Scripts/AStar/AStar.cs
+++ Assets/Scripts/AStar/AStar.cs
@@ -8,23 +8,19 @@ public class AStar : MonoBehaviour
     {
         //Setting up things required to do the algorithim
         List<Node_AStar> nodesToProcess = new List<Node_AStar>();
-        bool[] visited = new bool[graph.Length];
-        int[] fromNode = new int[graph.Length];
-        float[] gCost = new float[graph.Length];
-        float[] fCost = new float[graph.Length];
         bool createPath = false;
         
         nodesToProcess.Add(start);
-        for (int i = 0; i < graph.Length; i++)
-        {
-            visited[i] = false;
-            gCost[i] = int.MaxValue;
-            fCost[i] = int.MaxValue;
-            fromNode[i] = -1;
-        }
-        gCost[start.myIndex] = 0;
-        start.myHCost = Vector3.Distance(start.transform.position, target.transform.position);
-        fCost[start.myIndex] = start.myHCost;
+        // foreach (Node_AStar node in allNodes)
+        // {
+        //     node.visited = false;
+        //     node.gcost = float.MaxValue;
+        //     node.fCost = float.MaxValue;
+        //     node.fromNodeIndex = null;
+        // }
+        start.gcost = 0;
+        start.hCost = Vector3.Distance(start.transform.position, target.transform.position);
+        start.fCost = start.hCost;
 
         //Filling in the shortest distances from start and from nodes i.e processing each node
         while (nodesToProcess.Count > 0)
@@ -33,10 +29,10 @@ public class AStar : MonoBehaviour
             Node_AStar current = null;
             foreach(Node_AStar node in nodesToProcess)
             {
-                if(fCost[node.myIndex] < least)
+                if(node.fCost < least)
                 {
                     current = node;
-                    least = fCost[node.myIndex];
+                    least = node.fCost;
                 }
             }
 
@@ -50,37 +46,39 @@ public class AStar : MonoBehaviour
 
             for (int i = 0; i < current.connections.Count; i++)
             {
-                if(!visited[current.connections[i].myIndex])
+                if(!current.connections[i].visited)
                 {
                     float temp;
-                    temp = gCost[current.myIndex] + graph[current.myIndex][current.connections[i].myIndex];
-                    if(temp < gCost[current.connections[i].myIndex])
+                    temp = current.gcost + graph[current.myIndex][current.connections[i].myIndex];
+                    if(temp < current.connections[i].gcost)
                     {
-                        gCost[current.connections[i].myIndex] = temp;
-                        fromNode[current.connections[i].myIndex] = current.myIndex;
+                        current.connections[i].gcost= temp;
+                        current.connections[i].fromNodeIndex = current;
 
-                        current.connections[i].myHCost = Vector3.Distance(current.connections[i].transform.position, target.transform.position);
-                        fCost[current.connections[i].myIndex] = gCost[current.connections[i].myIndex] + current.connections[i].myHCost;
+                        current.connections[i].hCost = Vector3.Distance(current.connections[i].transform.position, target.transform.position);
+                        current.connections[i].fCost = current.connections[i].gcost + current.connections[i].hCost;
 
                         if(!nodesToProcess.Contains(current.connections[i]))
                             nodesToProcess.Add(current.connections[i]);
                     }
                 }
             }
-            visited[current.myIndex] = true;
+            current.visited = true;
         }
 
         if(createPath)
         {
             //Finding the finalPath
             Stack<int> path = new Stack<int>();
-            int currentIndex = target.myIndex;
-            if(fromNode[currentIndex] >= 0 || currentIndex == start.myIndex)
+            Node_AStar currentNode = target;
+            int currentIndex = currentNode.myIndex;
+            if(currentIndex >= 0 || currentIndex == start.myIndex)
             {
-                while (currentIndex >= 0)
+                while (currentNode != null)
                 {
+                    currentIndex = currentNode.myIndex;
                     path.Push(currentIndex);
-                    currentIndex = fromNode[currentIndex];
+                    currentNode =  currentNode.fromNodeIndex;
                 }
             }
 
--- Assets/Scripts/AStar/Node_AStar.cs
+++ Assets/Scripts/AStar/Node_AStar.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Node_AStar : MonoBehaviour
 {
-    public float myHCost;
+    public bool visited;
+    public Node_AStar fromNodeIndex;
+    public float gcost;
+    public float hCost;
+    public float fCost;
+
     public int myIndex;
     public List<Node_AStar> connections;
 
     public Node_AStar()
     {
         connections = new List<Node_AStar>();
+    }
+
+    private void Awake() 
+    {
+        visited = false;
+        fromNodeIndex = null;
+        gcost = float.MaxValue;
+        fCost = float.MaxValue;
     }
 }
