82dafabce01f5e5a61f13ce57a2602f6a8bdce9e
BlankRip <aniketh_g5@outlook.com>
2020-08-13 00:41:39 +0400

    Doing Forward kinematics before doing IK

---
+++ Assets/Scripts/IK/Segment.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment
+{
+    public Vector2 start;
+    public float length;
+    public float angle;
+    public float selfAngle;
+    public Vector2 end;
+    public Segment parent;
+
+#region Constructors
+    public Segment(float x, float y, float segmentLength, float initialAngle) {
+        start = new Vector2(x, y);
+        length = segmentLength;
+        angle = selfAngle = initialAngle;
+        parent = null;
+        CalculateSegmentEnd();
+    }
+
+    public Segment(Vector2 startPos, float segmentLength, float initialAngle) {
+        start = startPos;
+        length = segmentLength;
+        angle = selfAngle = initialAngle;
+        parent = null;
+        CalculateSegmentEnd();
+    }
+
+    public Segment(Segment parent, float segmentLength, float initialAngle) {
+        start = parent.end;
+        length = segmentLength;
+        angle = selfAngle = initialAngle;
+        this.parent = parent;
+        CalculateSegmentEnd();
+    }
+#endregion
+
+    public void CalculateSegmentEnd() {
+        float dx = length * Mathf.Cos(angle);
+        float dy = length * Mathf.Sign(angle);
+
+        end = new Vector2(start.x + dx, start.y + dy);
+    }
+
+    public void UpdateSegment() {
+        angle = selfAngle;
+        if(parent != null) {
+            start = parent.end;
+            angle += parent.angle;
+        }
+        CalculateSegmentEnd();
+    }
+
+    public void ChangeSegmentAngle(float changeInAngle) {
+        selfAngle += changeInAngle;
+    }
+
+    public void DrawSegment() {
+        Debug.DrawLine(new Vector3(start.x, start.y, 0), new Vector3(end.x, end.y, 0), Color.green);
+    }
+
+}
---
+++ Assets/Scripts/IK/TestKinematics.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class TestKinematics : MonoBehaviour
+{
+    Segment testSeg1;
+    Segment testSeg2;
+    Segment[] testSegments;
+    int clicks = 0;
+
+    private void Start() {
+        // testSeg1 = new Segment(0,0,3, 30 * Mathf.Deg2Rad);
+        // testSeg2 = new Segment(testSeg1 ,3, 30 * Mathf.Deg2Rad);
+        testSegments = new Segment[20];
+        testSegments[0] = new Segment(0,-15, 5, 30 * Mathf.Deg2Rad);
+        for (int i = 1; i < testSegments.Length; i++)
+            testSegments[i] = new Segment(testSegments[i-1] ,1, 30 * Mathf.Deg2Rad);
+    }
+
+    private void Update() {
+        //testSeg1.ChangeSegmentAngle(1 * Mathf.Deg2Rad);
+        // testSeg1.UpdateSegment();
+        // testSeg1.DrawSegment();
+        //testSeg2.ChangeSegmentAngle(3 * Mathf.Deg2Rad);
+        // testSeg2.UpdateSegment();
+        // testSeg2.DrawSegment();
+
+        // if(Input.GetKey(KeyCode.W)) {
+        //     clicks++;
+        //     Debug.Log(clicks);
+        //     testSeg1.ChangeSegmentAngle(1 * Mathf.Deg2Rad);
+        // }
+
+        for (int i = 0; i < testSegments.Length; i++) {
+            testSegments[i].UpdateSegment();
+            float randomAngelChange = Random.Range(0.3f, 3);
+            testSegments[i].ChangeSegmentAngle(randomAngelChange * Mathf.Deg2Rad);
+            testSegments[i].DrawSegment();
+        }
+    }
+}
