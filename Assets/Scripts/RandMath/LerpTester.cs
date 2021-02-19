1c1e4f9620413301cfd947b87b4f2126857fd216
BlankRip <aniketh_g5@outlook.com>
2021-02-19 12:59:04 +0400

    Idk tf I did here

---
+++ Assets/Scripts/RandMath/LerpTester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LerpTester : MonoBehaviour
+{
+    [SerializeField] Transform a;
+    [SerializeField] Transform b;
+    [Range(0, 1)] [SerializeField] float t;
+    private void Update() {
+        transform.position = Vector3.Lerp(a.position, b.position, t);
+    }
+}
---
+++ Assets/SomeTestings/TriggerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            Debug.Log("in");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            Debug.Log("out");
+        }
+    }
+}
