0a3209ef85dae5e41db94d8191d1fc56ed118f27
BlankRip <aniketh_g5@outlook.com>
2021-04-12 14:27:22 +0400

    Start making game to put a leaderboard

---
+++ Assets/Scripts/LeaderBoard/LB_GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LB_GameManager : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+
+    public int score;
+
+    private void Start() {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
+
+    public void AddScore() {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
