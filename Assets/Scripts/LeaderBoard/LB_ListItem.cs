4210f630aee521c2068b97cec2b4d3fb3ca87a4c
BlankRip <aniketh_g5@outlook.com>
2021-04-13 19:27:23 +0400

    Start setting up leaderboard UI

---
+++ Assets/Scripts/LeaderBoard/LB_ListItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LB_ListItem : MonoBehaviour
+{
+    [SerializeField] Text lbRank;
+    [SerializeField] Text username;
+    [SerializeField] Text score;
+
+    public void UpdateData(string rank, string name, string playerScore) {
+        lbRank.text = rank;
+        username.text = name;
+        score.text = playerScore;
+    }
+}
